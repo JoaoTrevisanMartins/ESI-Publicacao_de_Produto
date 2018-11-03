51bb2a93f18ae657423d1ba4c6d3c16e148123fe
lucaslai10 <lucaslai10>
2018-11-03 18:47:44 -0300

    cadastrador limpa_campos funcionando

--- src/Cadastrador/CadastradorHandler.java
+++ src/Cadastrador/CadastradorHandler.java
@@ -12,19 +12,91 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class CadastradorHandler  implements RequestHandler<Request, Response>{
+	
+	public static void main(String []args) throws Exception {
+		String descricao = "asdsdf@usp.br";
+		boolean aprovado = limpa_campos(descricao);
+		if(aprovado) {
+			System.out.println("Aprovado");
+		}
+		else {
+			System.out.println("Nooooob");
+		}
+	}
+	
     @Override
     public Response handleRequest(Request request, Context context) {
+    	boolean aprovado = false;
+    	try {
+			aprovado = limpa_campos(request.nome + " " + request.descricao);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	if(aprovado) {
+    		JSONObject data = new JSONObject();
+            try {
+				data.put("nome", request.nome);
+				data.put("foto", request.foto);
+				data.put("marca", request.marca);
+				data.put("tamanho", request.tamanho);
+				data.put("cor", request.cor);
+				data.put("departamento", request.departamento);
+				data.put("descricao", request.descricao);
+				data.put("categoria", request.categoria);
+				data.put("tendencias", request.tendencias);
+				data.put("frete", request.frete);
+				data.put("condicao", request.condicao);
+				data.put("preco", request.preco);
+				data.put("desconto", request.desconto);
+				URL url = new URL("https://dnetix.co/ping");
+	            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+	            httpConnection.setDoOutput(true);
+	            httpConnection.setRequestMethod("POST");
+	            httpConnection.setRequestProperty("Content-Type", "application/json");
+	            httpConnection.setRequestProperty("Accept", "application/json");
+	            
+	            DataOutputStream wr = new DataOutputStream(httpConnection.getOutputStream());
+	            wr.write(data.toString().getBytes());
+	            Integer responseCode = httpConnection.getResponseCode();
+	            
+	            
+	            BufferedReader bufferedReader;
+	            // Creates a reader buffer
+	            if (responseCode > 199 && responseCode < 300) {
+	                bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+	            } else {
+	            	bufferedReader = new BufferedReader(new InputStreamReader(httpConnection.getErrorStream()));
+	            }
 
+	            // To receive the response
+	            StringBuilder content = new StringBuilder();
+	            String line;
+	            while ((line = bufferedReader.readLine()) != null) {
+	                content.append(line).append("\n");
+	            }
+	            bufferedReader.close();
+	            JSONObject answer = new JSONObject(content);
+	            String a = answer.getString("response");
+	            if(a == "ok") {
+	            	return new Response("Cadastro realizado com sucesso", "aprovado");
+	            }
+	            
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+            
+    	}
 
-
-        return null;
+        return new Response("Cadastro nao sucedido", "reprovado");
     }
 
     static public boolean limpa_campos(String s) throws Exception{
         JSONObject data = new JSONObject();
         data.put("description", s);
         
-        URL url = new URL("https://dnetix.co/ping");
+        URL url = new URL("https://3pf8dr4ds6.execute-api.us-east-1.amazonaws.com/ChecaEmail");
         HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
         httpConnection.setDoOutput(true);
         httpConnection.setRequestMethod("POST");
@@ -51,14 +123,17 @@ public class CadastradorHandler  implements RequestHandler<Request, Response>{
             content.append(line).append("\n");
         }
         bufferedReader.close();
+        System.out.println(content.toString());
         
-        JSONObject answer = new JSONObject(line);
+        JSONObject answer = new JSONObject(content.toString());
         String a = answer.getString("response");
         if(a.equals("se fudeu arrombado")){
-        	
+        	return false;
         }
         return true;
     }
+    
+    
 
 
 }
